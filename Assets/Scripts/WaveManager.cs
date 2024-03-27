0cd1264bbda1a36ed335efa69818e939eec30dcb
Matthew Whelan Keats <mattdragon03@gmail.com>
2024-03-27 11:58:41 -0600

    Added basic wave spawning functionality

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -1,15 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class WaveManager : MonoBehaviour
 {
-    int currentWave = 1;
-    public GameObject enemy;
+    public int currentWave = 1;
+    public GameObject[] enemyPrefabs;
     Timer timer;
     public float spawnCooldown;
     public float spawnRadius;
+    int currentEnemy = 0;
+    public GameObject CardMenu;
+
+    public EnemyTypeWrapper[] waves; //Fill this with the enemies in each waves
 
     private void Start()
     {
@@ -24,9 +30,22 @@ public class WaveManager : MonoBehaviour
 
     public void SpawnEnemy()
     {
-        timer.Reset();
-        Vector3 pos = RandomNavmeshLocation(spawnRadius);
-        Instantiate(enemy, pos, Quaternion.Euler(Vector3.zero), transform);
+        currentEnemy++;
+        if (currentEnemy >= waves[currentWave - 1].enemies.Length) //If the player is on the last enemy of the wave, go to the next wave
+        {
+            currentWave = Mathf.Clamp(currentWave + 1, 1, waves.Length);
+            currentEnemy = 1;
+            CardMenu.SetActive(true);
+        }
+        else
+        {
+            timer.Reset();
+            Vector3 pos = RandomNavmeshLocation(spawnRadius);
+            GameObject selectedPrefab = enemyPrefabs[(int)(waves[currentWave - 1].enemies[currentEnemy - 1])]; //Selects an enemy prefab based on the enum
+
+            Instantiate(selectedPrefab, pos, Quaternion.Euler(Vector3.zero), transform);
+
+        }
     }
 
     public Vector3 RandomNavmeshLocation(float radius)
@@ -41,4 +60,17 @@ public class WaveManager : MonoBehaviour
         }
         return finalPosition;
     }
+}
+
+[Serializable]
+public enum EnemyTypes
+{
+    Standard,
+    Projectile
+}
+
+[Serializable]
+public struct EnemyTypeWrapper
+{
+    public EnemyTypes[] enemies;
 }
