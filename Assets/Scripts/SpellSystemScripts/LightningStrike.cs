5e61a3cb3d321310986e194446851aede070323c
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2024-04-09 17:16:52 -0600

    Spell System changes and additions
    Fixed:
    - Lightning Strike
    - Toxic Cloud
    - DeckSystemScript
    
    Added:
    - Warped World: slows all enemy movement speed by half
    - LayerMask: Enemy, this is meant for spells to interact with, notably the Lightning Strike spell

--- Assets/Scripts/SpellSystemScripts/LightningStrike.cs
+++ Assets/Scripts/SpellSystemScripts/LightningStrike.cs
@@ -6,11 +6,15 @@ public class LightningStrike : MonoBehaviour
 {
     public float targetScale = 2f;
     public float duration = 0.2f;
+    public float damageRadius = 5f;
+    public LayerMask enemyLayer;
 
     private Vector3 initialScale;
     private float startTime;
-
     private float damageDeal = 10f;
+
+    private bool canDamage = true;
+    private float damageCooldown = 0.1f;
 
     private PlayerController playerScript;
 
@@ -25,11 +29,8 @@ public class LightningStrike : MonoBehaviour
     void Update()
     {
         float progress = (Time.time - startTime) / duration;
-
         progress = Mathf.Clamp01(progress);
-
         Vector3 newScale = Vector3.Lerp(initialScale, new Vector3(targetScale, targetScale, targetScale), progress);
-
         transform.localScale = newScale;
 
         if (progress >= 1f)
@@ -40,11 +41,27 @@ public class LightningStrike : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        HealthScript enemyGO = other.gameObject.GetComponent<HealthScript>();
-
-        if (enemyGO != null)
+        if (canDamage)
         {
-            enemyGO.health -= damageDeal /** (playerScript.gun.stats.damage.Value / playerScript.gun.stats.damage.BaseValue)*/;
+            Collider[] colliders = Physics.OverlapSphere(transform.position, damageRadius, enemyLayer);
+
+            foreach (Collider collider in colliders)
+            {
+                HealthScript enemyHealth = collider.GetComponent<HealthScript>();
+                if (enemyHealth != null)
+                {
+                    enemyHealth.health -= damageDeal /** * (playerScript.gun.stats.damage.Value / playerScript.gun.stats.damage.BaseValue)*/;
+                }
+            }
+
+            StartCoroutine(DamageCooldown());
         }
+    }
+
+    private IEnumerator DamageCooldown()
+    {
+        canDamage = false;
+        yield return new WaitForSeconds(damageCooldown);
+        canDamage = true;
     }
 }
